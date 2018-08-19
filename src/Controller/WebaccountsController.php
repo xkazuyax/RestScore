d5c9fede90c929506ea84c5dc496a59df1be3c00
Kazuya Yanagi <yanagi200@docomo.ne.jp>
2018-08-20 02:44:31 +0900

    Webアカウント更新画面

--- src/Controller/WebaccountsController.php
+++ src/Controller/WebaccountsController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controller;
 
+use \Exception;
+use Cake\Log\Log;
+
 class WebaccountsController extends AppController {
 	public function initialize() {
 		$this->autoRender = true;
@@ -123,12 +126,77 @@ class WebaccountsController extends AppController {
 			$this->request->session()->destroy();
 			$this->redirect(["controller" => "Webaccounts","action" => "index"]);
 		}
-		$webaccount = $this->Webaccounts->find("all",[
+
+		$webaccount_data = $this->Webaccounts->find("all",[
 			"conditions" => [
 				"id" => $id
 			]
 		]);
+		$this->set("webaccount_data",$webaccount_data);
+		$this->set("id",$id);
+		try {
 		$this->set("entity",$this->Webaccounts->get($id));
+		} catch(Exception $e) {
+			Logg::write("debug",$e->getMessage());
+		}
+
+	}
+
+	public function updateCheck() {
+		if (!$this->request->session()->read("web_id")) {
+			$this->request->session()->destroy();
+			$this->redirect(["controller" => "Webaccounts","action" => "index"]);
+		}
+		$error = "";
+		if ($this->request->is('put')) {
+			$new_entity = $this->Webaccounts->newEntity($this->request->data);
+			try {
+				$entity = $this->Webaccounts->get($this->request->data['id']);
+			} catch(Exception $e) {
+				Logg:write("debug",$e->getMessage());
+			}
+			if ($this->request->data['pass'] == $this->request->data['pass2']) {
+				//パスワードが入力されている場合のみ、Entityに追加
+				if ($this->request->data["pass"]) {
+					$entity->password = $this->request->data['pass'];
+				}
+				$upload_file = $_FILES["image_path"];
+				//アップロードファイルがある場合のみ、Entityに追加
+				if ($upload_file) {
+					//ファイルが正しくアップロードされているか
+					if ($upload_file["size"] > 0 && $upload_file["tmp_name"]) {
+						//ファイルを保存
+						$file_path ="/var/www/html/img/".$upload_file["name"];
+						if (move_uploaded_file($upload_file["tmp_name"],$file_path)) {
+							if ($upload_file['name'] != $entity->image_name) {
+								unlink('/var/www/html/img/'.$entity->image_name);
+							}
+							$entity->image_name = $upload_file["name"];
+						}
+					}
+				}
+			} else {
+				$error = "パスワードが一致しません";
+			}
+			$entity->modified_date = time();
+			$this->Webaccounts->patchEntity($entity,$this->request->data);
+			if ($this->Webaccounts->save($entity)) {
+				$this->redirect(["controller" => "webaccounts","action" => "updateComplete"]);
+			}
+		} else {
+			$new_entity = $this->Form->newEntity();
+
+		}
+		$this->set("error",$error);
+		$this->set("id",$this->request->data['id']);
+		$this->set("entity",$new_entity);
+	}
+
+	public function updateComplete() {
+		if (!$this->request->session()->read("web_id")) {
+			$this->request->session()->destroy();
+			$this->redirect(["controller" => "Webaccounts","action" => "index"]);
+		}
 	}
 }
 ?>
---
+++ src/Template/Webaccounts/update_complete.ctp
@@ -0,0 +1,22 @@
+<div class="card m-5">
+	<div class="card-header">
+		<div class="row">
+			<div class="col-sm-12">
+				<h4 class="text-center">アカウント更新完了</h4>
+			</div>
+		</div>
+	</div>
+	<div class="card-body">
+		<div class="row">
+			<div class="col-sm-12 m-5">
+				<h4 class="text-center">アカウントの更新が完了しました</h4>
+			</div>
+			<div class="row">
+			<div class="col-sm-3"></div>
+				<div class="col-sm-6">
+					<a class="btn btn-info center-block" href="<?=$this->Url->build('/Webaccounts/webList');?>">戻る</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
